61c881ae34f9af7485c3ae03e45dee0e823775e7
Lousy Lynx <mttprvstanddad@gmail.com>
2017-08-10 20:25:33 -0500

    Update argument parser to retrieve the file

--- src/main/java/com/lousylynx/asciidots/cli/ADCLI.java
+++ src/main/java/com/lousylynx/asciidots/cli/ADCLI.java
@@ -9,22 +9,15 @@ public class ADCLI {
     public static void main(String[] args) {
         handleArguments(args);
 
-        if(exit)
+        if (exit)
             System.exit(0);
     }
 
     private static void handleArguments(String[] args) {
         Options options = new Options();
 
-
-
-//        options.addOption(Option.builder()
-//                .longOpt("file")
-//                .hasArg()
-//                .build());
-
-        options.addOption("file", true, FILE_OPTION);
-        options.addOption("help", false, "Print the help command");
+        options.addOption(null, "help", false, "Print the help command");
+        options.addOption("v", "version", false, "Prints the current version being ran");
         options.addOption("t", "ticks", false, "Run the program for a specified number of ticks");
         options.addOption("s", "silent", false, "Run without printing ANYTHING to the console. Useful for benchmarking");
         options.addOption("d", "debug", false, "Run the program in debug mode. It shows the program and highlights the dots with red. Press enter to step the program once.");
@@ -34,19 +27,49 @@ public class ADCLI {
         options.addOption("h", "head", false, "TODO: add description here");
 
         try {
-            CommandLine cmd = new DefaultParser().parse(options, args);
+            CommandLine cmd = new DefaultParser().parse(options, args, false);
 
-            if(cmd.hasOption("help")) {
+            if (cmd.hasOption("help")) {
                 HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp("asciidots", options);
+                formatter.printHelp("asciidots <FILE>", options, true);
 
                 exit = true;
                 return;
             }
 
-            System.out.println(cmd.getOptionValue("filename"));
+            if (cmd.hasOption("version")) {
+                // TODO: make this non hardcoded
+                System.out.println("Current version is: 1.0");
+
+                exit = true;
+                return;
+            }
+
+            if (cmd.getArgs().length > 1) {
+                System.out.println("Unknown stray arguments: " + String.join(", ", cmd.getArgList()));
+
+                exit = true;
+                return;
+            } else if (cmd.getArgs().length < 1) {
+                System.out.println("You must specify a file to run.");
+
+                exit = true;
+                return;
+            }
+
+            String program = getFile(cmd);
+
+            // TODO: Run the interpreter from here
         } catch (ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getFile(CommandLine cmd) {
+        String file = cmd.getArgs()[0];
+
+        // TODO: continue reading the file and output the contents
+
+        return file;
     }
 }
